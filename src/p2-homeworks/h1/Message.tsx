4e83b22bb7df013990e7bf57c79307408214d5f1
Helen <melnikelena6969@gmail.com>
2020-08-24 13:32:10 +0300

    01Homework

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React from 'react';
+import style from './Message.module.css';
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <div className={style.wrapperMessage}>
+            <img src={props.avatar} className={style.avatar}/>
+            <div className={style.message}>
+                <div className={style.fromUser}>{props.name}</div>
+                <div>
+                    <span className={style.messageText}>{props.message}</span>
+                    <span className={style.time}>{props.time}</span>
+                </div>
+            </div>
 
         </div>
     );
